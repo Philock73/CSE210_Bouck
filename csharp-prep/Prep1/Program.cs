0f8d7a1767eeb8916629957ffa802e2476ede429
Nathan Bouck <natebouck@gmail.com>
2025-04-26 11:33:01 -0600

    C# Prep1 assignment done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Hello, please give us your first name: ");
+        string givenname = Console.ReadLine();
+        Console.Write("Please now enter your last name: ");
+        string secondname = Console.ReadLine();
+        Console.WriteLine($"Welcome {secondname}, {givenname} {secondname}.");
     }
 }
