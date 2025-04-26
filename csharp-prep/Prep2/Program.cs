18719d3e62f46e257cde96bbf2b9e1c276cc31cb
Nathan Bouck <natebouck@gmail.com>
2025-04-26 12:02:53 -0600

    C# Prep 2 Complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Thank you for using us for finding your grade. Please what percentage did you get?");
+        string letter = Console.ReadLine();
+        int grade = int.Parse(letter);
+        string credit = "No grade";
+        if (grade >= 70)
+        {
+            credit = "passed";
+            if (grade >= 90)
+            {
+                letter = "an A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "a B";
+            }
+            else
+            {
+                letter = "a C";
+            }
+        }
+        else
+        {
+            credit = "failed";
+            if (grade >= 60)
+            {
+                letter = "a D";
+            }
+            else
+            {
+                letter = "a F";
+            }
+
+        }
+        Console.WriteLine($"You {credit} with {letter}");
     }
 }
