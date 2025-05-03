c13a2fc0c440c4a6c1c892db92e1af8da6c5bdaf
Nathan Bouck <natebouck@gmail.com>
2025-05-03 17:08:44 -0600

    Completed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,44 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int ChangeString(string word)
+    {
+        int numb = int.Parse(word);
+        return numb;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is a number:");
+        string num = Console.ReadLine();
+        int number = ChangeString(num);
+        return number;
+    }
+    static int SquareNumber(int one)
+    {
+        int squared = one * one;
+        return squared;
+    }
+    static void DisplayResult()
+    {   
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+        Console.WriteLine($"{name}, {number} squared is {squared}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult();
     }
 }
