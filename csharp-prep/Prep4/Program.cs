3a258548928a816f1cfe2e4d2d1c3bf179a22e07
Nathan Bouck <natebouck@gmail.com>
2025-05-02 21:03:31 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,33 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   int listItem = 1;
+        List<int> numbers = new List<int>();
+        int i = 0;
+        int big = 0;
+        int total = 0;
+        int mean = 0; 
+        Console.WriteLine("Please keep entering numbers and when you are finished put a 0, thanks.");
+        do
+        {
+        string newItem = Console.ReadLine();
+        listItem = int.Parse(newItem);
+        numbers.Add(listItem);
+        }while (listItem != 0);
+        numbers.Remove(0);
+        foreach (int input in numbers)
+        {
+            Console.WriteLine(input);
+            total = total + input;
+            i = i +1;
+            mean = total / i;
+            if (input > big)
+            {
+                big = input;
+            }
+        }
+        Console.WriteLine($"Your Sum is {total}");
+        Console.WriteLine($"Your average is {mean}");
+        Console.WriteLine($"Your largest number is {big}");
     }
 }
