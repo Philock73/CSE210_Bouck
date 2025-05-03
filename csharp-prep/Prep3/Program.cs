383764c00bc788523189a9cbfa92399851b0a1a0
Nathan Bouck <natebouck@gmail.com>
2025-05-02 19:46:45 -0600

    completed Prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,28 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(0,9);
+        int number = randomGenerator.Next(0,9);
+        Console.WriteLine("Please enter the two digit password:");
+        string guess = Console.ReadLine();
+        string realPassword = $"{magic}{number}";
+        int pass = int.Parse(realPassword);
+        int weird = int.Parse(guess);
+        while (guess != realPassword)
+         {   if (weird > pass)
+            {
+                Console.WriteLine("The password is Lower");
+            }
+            else
+            {
+                Console.WriteLine("The password is Higher");
+            } 
+            Console.WriteLine("Try Again:");
+            guess = Console.ReadLine();
+            weird = int.Parse(guess);
+        }
+        Console.WriteLine("You got it correct!");
     }
 }
